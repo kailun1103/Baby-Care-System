b0f2870f278c8ba7dbd788d17ce650f2fb025d43
kailun1103 <kailunchang1103@gmail.com>
2024-05-22 15:18:24 +0800

    add tensorflow streaming function for detecting baby status

--- app.py
+++ app.py
@@ -1,30 +1,52 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from flask import Flask, render_template, Response
+from flask import Flask, Response, render_template
 import cv2
+import tensorflow as tf
+import numpy as np
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)
 
-def gen_frames():
+model = tf.keras.models.load_model('keras_model.h5', compile=False)  # 載入模型
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)          # 設定資料陣列
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        raise RuntimeError("Cannot open camera")
+
     while True:
-        success, frame = camera.read()
-        if not success:
+        ret, frame = cap.read()
+        if not ret:
             break
+
+        # 左右翻轉影像
+        frame = cv2.flip(frame, 1)
+
+        img = cv2.resize(frame, (398, 224))
+        img = img[0:224, 80:304]
+        image_array = np.asarray(img)
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+        data[0] = normalized_image_array
+        prediction = model.predict(data)
+        a, b = prediction[0]
+        if a > 0.6:
+            text = 'ok~'
+        elif b > 0.6:
+            text = 'Danger!!'
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            text = 'Processing...'
+        cv2.putText(img, text, (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        ret, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    app.run(debug=True, threaded=True)
